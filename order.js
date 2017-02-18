80d0b1e6dfe56837cf9dd569b8bd595b4e95cde0
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-02-18 15:33:33 -0700

    multiple order ideas

--- order.js
+++ order.js
@@ -1,19 +1,50 @@
 // create table rows function
 $(document).ready(function() {
+    let counter = 0
+
     function createTableRow(tbody, price, itemName) {
-        let tableRow = $('<tr>')
-        let tableData = $('<td>')
-        let priceData = $('<td>')
-        tableRow.appendTo('#tbody')
-        tableData.appendTo(tableRow)
-        priceData.appendTo(tableRow)
-        tableData.text(itemName)
-        priceData.text(price)
+        if (counter === 0) {
+            let tableRow = $('<tr>')
+            let tableData = $('<td>').addClass(itemName + "-data")
+            let priceData = $('<td>').addClass(price + '-data')
+            tableRow.appendTo('#tbody')
+            tableData.appendTo(tableRow)
+            priceData.appendTo(tableRow)
+
+            tableData.text(itemName)
+            priceData.text(price)
+            console.log(tableData[0]);
+        } else if (counter >= 1) {
+            console.log($('t').children().attr(itemName + "-data"));
+            $('tr').children().attr(itemName + "-data").text().replaceWith(itemName + " x" + counter)
+            $('tr').children().hasClass(price + "-data").replaceWith("$" + (price.slice(1) * counter))
+        }
     }
+
+    // function replaceTableRow(price, item) {
+    //   createTableRow()
+    //     $(tableData.text()).replaceWith(item)
+    //     $(priceData.text()).replaceWith(price)
+    // }
+    // replaceTableRow(tbody, '$10.99', "Sesame Chicken", 3)
+    console.log(counter);
+
     $('.order-event').on('click', function() {
         if (event.target.className === 'order-link') {
-            console.log("you clicked");
+            let price = $(event.target).siblings().text()
+            let item = $(event.target).parents().parents().children(".card-image").children('.card-title').text();
+            createTableRow(tbody, price, item)
+            counter = counter + 1;
 
+            // if (counter <= 1) {
+            //     createTableRow(tbody, price, item)
+            //
+            // } else if (counter > 1) {
+            //     price = "$" + (price.slice(1) * counter)
+            //     item = item + " x" + counter
+            //     replaceTableRow(price, item)
+            // }
+            console.log("counter = " + counter);
         }
 
 
@@ -22,7 +53,6 @@ $(document).ready(function() {
 
 
 
-    createTableRow(tbody, 9, "hello");
 
 
 
